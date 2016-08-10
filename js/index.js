47a1ae6f7737e504e915c4850e7a1e873377524b
implementationexpenzing <implementation@expenzing.com>
2016-08-10 19:06:55 +0530

    IDBI Related
    idbi related

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
-var urlPath='http://14.140.170.141/idbi_tne_uat/WebService/Login';
+var urlPath='http://14.140.170.141:80/idbi_tne_uat/WebService/Login/';
 var WebServicePath = 'http://live.nexstepapps.com:8284/NexstepWebService/mobileLinkResolver.service';
 var clickedFlagCar = false;
 var clickedFlagTicket = false;

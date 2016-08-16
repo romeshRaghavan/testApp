777b6ac400b3b0bd117df796144b6dee9f0a6900
implementationexpenzing <implementation@expenzing.com>
2016-08-16 10:36:39 +0530

    revert idbi changes
    revert idbi changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
-var urlPath='http://14.140.170.141:80/idbi_tne_uat/WebService/Login/';
+var urlPath;
 var WebServicePath = 'http://live.nexstepapps.com:8284/NexstepWebService/mobileLinkResolver.service';
 var clickedFlagCar = false;
 var clickedFlagTicket = false;
@@ -45,8 +45,8 @@ function login()
     jsonToBeSend["pass"] = password.value;
    	var headerBackBtn=defaultPagePath+'categoryMsgPage.html';
 	var pageRef=defaultPagePath+'category.html';
-	setUrlPathLocalStorage(urlPath);
-	//urlPath=window.localStorage.getItem("urlPath");
+	//setUrlPathLocalStorage(urlPath);
+	urlPath=window.localStorage.getItem("urlPath");
 	j('#loading').show();
     j.ajax({
          url: urlPath+"LoginWebService",

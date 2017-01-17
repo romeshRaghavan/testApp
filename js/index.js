d991cab535bb9567c02a0c36ef8e9658bd795415
implementationexpenzing <implementation@expenzing.com>
2017-01-17 10:19:35 +0530

    Change For Unit Fix By Kunal
    Change For Unit Fix By Kunal

--- js/index.js
+++ js/index.js
@@ -1240,105 +1240,99 @@ function onloadTimePicker(){
 	 }
 
 function setPerUnitDetails(transaction, results){
- 		 
-    	if(results!=null){
-		        var row = results.rows.item(0);
-		        perUnitDetailsJSON["expenseIsfromAndToReqd"]=row.expIsFromToReq;
-		        perUnitDetailsJSON["isUnitReqd"]=row.expIsUnitReq;
-		        perUnitDetailsJSON["expRatePerUnit"]=row.expRatePerUnit;
-		        perUnitDetailsJSON["expFixedOrVariable"]=row.expFixedOrVariable;
-		        perUnitDetailsJSON["expFixedLimitAmt"]=row.expFixedLimitAmt;
-		        perUnitDetailsJSON["expenseName"]=row.expName;
-				perUnitDetailsJSON["expPerUnitActiveInative"]=row.expPerUnitActiveInative;
-				perUnitDetailsJSON["isErReqd"]=row.isErReqd;
-				perUnitDetailsJSON["limitAmountForER"]=row.limitAmountForER;
-		        document.getElementById("ratePerUnit").value = row.expRatePerUnit;
-		        document.getElementById("expAmt").value="";
-		        document.getElementById("expUnit").value="";
-			document.getElementById("expFromLoc").value = "";
-			document.getElementById("expToLoc").value = "";
-			document.getElementById("expNarration").value = "";
-			document.getElementById("expUnit").value = "";
-			document.getElementById("expAmt").value = "";
-		        if(perUnitDetailsJSON.expenseIsfromAndToReqd=='N'){
-					document.getElementById("expFromLoc").value="";
-					document.getElementById("expToLoc").value="";
-					document.getElementById("expFromLoc").disabled =true;
-					document.getElementById("expToLoc").disabled =true;
-					document.getElementById("expFromLoc").style.backgroundColor='#d1d1d1'; 
-					document.getElementById("expToLoc").style.backgroundColor='#d1d1d1';
-					document.getElementById("expNarration").disabled =false;
-					document.getElementById("expNarration").style.backgroundColor='#FFFFFF';
-					document.getElementById("mapImage").style.display= "none";
-				}else{
-					document.getElementById("expFromLoc").disabled =false;
-					document.getElementById("expToLoc").disabled =false;
-					document.getElementById("expFromLoc").value="";
-					document.getElementById("expToLoc").value="";
-					document.getElementById("expNarration").value="";
-					document.getElementById("expFromLoc").style.backgroundColor='#FFFFFF'; 
-					document.getElementById("expToLoc").style.backgroundColor='#FFFFFF'; 
-					//alert(window.localStorage.getItem("MobileMapRole"))
-					if(window.localStorage.getItem("MobileMapRole") == 'true') 
-					{
-						attachGoogleSearchBox(document.getElementById("expFromLoc"));
-						attachGoogleSearchBox(document.getElementById("expToLoc"));
-						document.getElementById("mapImage").style.display="";
-						document.getElementById("expNarration").disabled =true;
-						document.getElementById("expNarration").style.backgroundColor='#d1d1d1';
-					} 
-				}
-				if(perUnitDetailsJSON.isUnitReqd=='Y'){
-					document.getElementById("expAmt").value="";
-					if(perUnitDetailsJSON.expFixedOrVariable=='V'){
-						flagForUnitEnable = true;
-						if(window.localStorage.getItem("MobileMapRole") == 'true') 
-						{
-							document.getElementById("expAmt").disabled =false;
-							document.getElementById("expAmt").style.backgroundColor='#FFFFFF';
-						}
-						else
-						{
-							flagForUnitEnable = true;
-							if(window.localStorage.getItem("MobileMapRole") == 'true') 
-							{
-								document.getElementById("expUnit").disabled =true;
-								document.getElementById("expUnit").style.backgroundColor='#d1d1d1';
-							}
-							else{
-								document.getElementById("expUnit").disabled =false;
-								document.getElementById("expUnit").style.backgroundColor='#FFFFFF';
-							} 
-							document.getElementById("expAmt").disabled =true;
-							document.getElementById("expAmt").style.backgroundColor='#d1d1d1';
-						}
-					}else{
-						flagForUnitEnable = true;
-						document.getElementById("expUnit").disabled =false;
-						document.getElementById("expUnit").style.backgroundColor='#FFFFFF'; 
-						document.getElementById("expAmt").disabled =true;
-						document.getElementById("expAmt").style.backgroundColor='#d1d1d1'; 
-					}
-				}else{
-					flagForUnitEnable = false;
+	
+ 	if(results!=null){
+		var row = results.rows.item(0);
+		perUnitDetailsJSON["expenseIsfromAndToReqd"]=row.expIsFromToReq;
+		perUnitDetailsJSON["isUnitReqd"]=row.expIsUnitReq;
+		perUnitDetailsJSON["expRatePerUnit"]=row.expRatePerUnit;
+		perUnitDetailsJSON["expFixedOrVariable"]=row.expFixedOrVariable;
+		perUnitDetailsJSON["expFixedLimitAmt"]=row.expFixedLimitAmt;
+		perUnitDetailsJSON["expenseName"]=row.expName;
+		perUnitDetailsJSON["expPerUnitActiveInative"]=row.expPerUnitActiveInative;
+		perUnitDetailsJSON["isErReqd"]=row.isErReqd;
+		perUnitDetailsJSON["limitAmountForER"]=row.limitAmountForER;
+		document.getElementById("ratePerUnit").value = row.expRatePerUnit;
+		document.getElementById("expAmt").value="";
+		document.getElementById("expUnit").value="";
+		document.getElementById("expFromLoc").value = "";
+		document.getElementById("expToLoc").value = "";
+		document.getElementById("expNarration").value = "";
+		document.getElementById("expUnit").value = "";
+		document.getElementById("expAmt").value = "";
+		if(perUnitDetailsJSON.expenseIsfromAndToReqd=='N'){
+			document.getElementById("expFromLoc").value="";
+			document.getElementById("expToLoc").value="";
+			document.getElementById("expFromLoc").disabled =true;
+			document.getElementById("expToLoc").disabled =true;
+			document.getElementById("expFromLoc").style.backgroundColor='#d1d1d1'; 
+			document.getElementById("expToLoc").style.backgroundColor='#d1d1d1';
+			document.getElementById("expNarration").disabled =false;
+			document.getElementById("expNarration").style.backgroundColor='#FFFFFF';
+			document.getElementById("mapImage").style.display= "none";
+		}else{
+			document.getElementById("expFromLoc").disabled =false;
+			document.getElementById("expToLoc").disabled =false;
+			document.getElementById("expFromLoc").value="";
+			document.getElementById("expToLoc").value="";
+			document.getElementById("expNarration").value="";
+			document.getElementById("expFromLoc").style.backgroundColor='#FFFFFF'; 
+			document.getElementById("expToLoc").style.backgroundColor='#FFFFFF'; 
+			//alert(window.localStorage.getItem("MobileMapRole"))
+			if(window.localStorage.getItem("MobileMapRole") == 'true') 
+			{
+				attachGoogleSearchBox(document.getElementById("expFromLoc"));
+				attachGoogleSearchBox(document.getElementById("expToLoc"));
+				document.getElementById("mapImage").style.display="";
+				document.getElementById("expNarration").disabled =true;
+				document.getElementById("expNarration").style.backgroundColor='#d1d1d1';
+			} 
+		}
+		if(perUnitDetailsJSON.isUnitReqd=='Y'){
+			document.getElementById("expAmt").value="";
+			if(perUnitDetailsJSON.expFixedOrVariable=='V'){
+				flagForUnitEnable = true;
+				if(perUnitDetailsJSON.expenseIsfromAndToReqd=='Y' && window.localStorage.getItem("MobileMapRole") == 'true'){
 					document.getElementById("expUnit").disabled =true;
-					document.getElementById("expAmt").disabled =false;
-					document.getElementById("expAmt").style.backgroundColor='#FFFFFF'; 
-					document.getElementById("expUnit").style.backgroundColor='#d1d1d1'; 
-				}
-				if(perUnitDetailsJSON.expPerUnitActiveInative=='1'){
-					flagForUnitEnable=false;
-					document.getElementById("expUnit").disabled =true;
-					document.getElementById("expAmt").disabled =false;
-					document.getElementById("expAmt").style.backgroundColor='#FFFFFF'; 
 					document.getElementById("expUnit").style.backgroundColor='#d1d1d1';
 				}
+				else{
+					document.getElementById("expUnit").disabled =false;
+					document.getElementById("expUnit").style.backgroundColor='#FFFFFF';
+				}
+				document.getElementById("expAmt").disabled =false;
+				document.getElementById("expAmt").style.backgroundColor='#FFFFFF';
+			}else{
+				flagForUnitEnable = true;
+				if(perUnitDetailsJSON.expenseIsfromAndToReqd=='Y' && window.localStorage.getItem("MobileMapRole") == 'true'){
+					document.getElementById("expUnit").disabled =true;
+					document.getElementById("expUnit").style.backgroundColor='#d1d1d1';
+				}
+				else{
+					document.getElementById("expUnit").disabled =false;
+					document.getElementById("expUnit").style.backgroundColor='#FFFFFF';
+				}
+				document.getElementById("expAmt").disabled =true;
+				document.getElementById("expAmt").style.backgroundColor='#d1d1d1'; 
+			}
 		}else{
-
-			alert("Please Synch your expense Names to claim expense.");
+			flagForUnitEnable = false;
+			document.getElementById("expUnit").disabled =true;
+			document.getElementById("expUnit").style.backgroundColor='#d1d1d1'; 
+			document.getElementById("expAmt").disabled =false;
+			document.getElementById("expAmt").style.backgroundColor='#FFFFFF'; 
 		}
- 	
- 	}
+		if(perUnitDetailsJSON.expPerUnitActiveInative=='1'){
+			flagForUnitEnable=false;
+			document.getElementById("expUnit").disabled =true;
+			document.getElementById("expAmt").disabled =false;
+			document.getElementById("expAmt").style.backgroundColor='#FFFFFF'; 
+			document.getElementById("expUnit").style.backgroundColor='#d1d1d1';
+		}
+	}else{
+		alert("Please Synch your expense Names to claim expense.");
+	}
+ }
 
 
  	function setModeCategroyDetails(transaction, results){
